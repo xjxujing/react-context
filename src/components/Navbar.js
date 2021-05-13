4da2bdd91cade8e7e0490a7bf91974accbbcf513
xjxujing <xujing_xj@foxmail.com>
2021-05-13 16:38:18 +0800

    :init:

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h4>哈行啊</h4>
+        <ul>
+          <li>Home</li>
+          <li>Home1</li>
+          <li>Home2</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Navbar
