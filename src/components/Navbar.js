6686b3f2936cfc8625f53d78433c010b1dfce305
xjxujing <xujing_xj@foxmail.com>
2021-05-14 14:40:26 +0800

    使用 Consumer 获取 context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,32 @@ import React, { Component } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 
 class Navbar extends Component {
-  static contextType = ThemeContext
+  // static contextType = ThemeContext
 
   render() {
     // console.log(this.context)
-    const { isLightTheme, light, dark } = this.context
-    const theme = isLightTheme ? light : dark
     return (
-      <nav className="navBar"  style={{backgroundColor: theme.ui, color: theme.syntax}}>
-        <h1>Context App</h1>
-        <ul className="navItems">
-          <li>Home</li>
-          <li>Home1</li>
-          <li>Home2</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context
+        const theme = isLightTheme ? light : dark;
+
+        return (
+          <nav className="navBar" style={{ backgroundColor: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul className="navItems">
+              <li>Home</li>
+              <li>Home1</li>
+              <li>Home2</li>
+            </ul>
+          </nav>
+        )
+
+
+      }
+      }
+
+      </ThemeContext.Consumer >
+
     )
   }
 }
